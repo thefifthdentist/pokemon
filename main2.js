be70591af9d8257327db62dc97b20e970ccc4111
Mada Romero <37271419+thefifthdentist@users.noreply.github.com>
2018-04-24 10:17:43 -0400

    Committing via upload final
    Overwrite old files/finalize project for submissions

--- main2.js
+++ main2.js
@@ -16,7 +16,8 @@ class Petrova {
 }
 
 class Pokemon {
-    constructor(hp, attack, defense, abilities) {
+    constructor(name, hp, attack, defense, abilities) {
+        this.name = name
         this.hp = hp
         this.attack = attack
         this.defense = defense
@@ -34,12 +35,14 @@ axios.get("https://pokeapi.co/api/v2/pokemon/354/")
     let ul = document.querySelector("#banetteDiv")
 
     let pokeData = response.data
+    let name = document.createElement("li")
     let hp = document.createElement("li")
     let attack = document.createElement("li")
     let defense = document.createElement("li")
     let abilities = document.createElement("li")
 
     let banette = new Pokemon(
+        pokeData.name,
         pokeData.stats[5].base_stat,
         pokeData.stats[4].base_stat,
         pokeData.stats[3].base_stat,
@@ -51,11 +54,13 @@ axios.get("https://pokeapi.co/api/v2/pokemon/354/")
 
     pokeData.innerHTML = banette.data
 
+    name.innerHTML = "Name: " + banette.name
     hp.innerHTML = "HP: " + banette.hp
     attack.innerHTML = "ATTACK: " + banette.attack
     defense.innerHTML = "DEFENSE: " + banette.defense
     abilities.innerHTML = "ABILITIES: " + banette.abilities
 
+    ul.appendChild(name)
     ul.appendChild(hp)
     ul.appendChild(attack)
     ul.appendChild(defense)
@@ -72,16 +77,18 @@ axios.get("https://pokeapi.co/api/v2/pokemon/562/")
     let ul = document.querySelector("#yamaskDiv")
 
     let pokeData = response.data
+    let name = document.createElement("li")
     let hp = document.createElement("li")
     let attack = document.createElement("li")
     let defense = document.createElement("li")
     let abilities = document.createElement("li")
 
     let yamask = new Pokemon(
+        pokeData.name,
         pokeData.stats[5].base_stat,
         pokeData.stats[4].base_stat,
         pokeData.stats[3].base_stat,
-        pokeData.abilities[0].ability.name
+        pokeData.abilities[0].ability.name + ", " + pokeData.abilities[1].ability.name
     )
     console.log(yamask)
 
@@ -89,11 +96,13 @@ axios.get("https://pokeapi.co/api/v2/pokemon/562/")
 
     pokeData.innerHTML = yamask.data
 
+    name.innerHTML = "Name: " + yamask.name
     hp.innerHTML = "HP: " + yamask.hp
     attack.innerHTML = "ATTACK: " + yamask.attack
     defense.innerHTML = "DEFENSE: " + yamask.defense
     abilities.innerHTML = "ABILITIES: " + yamask.abilities
 
+    ul.appendChild(name)
     ul.appendChild(hp)
     ul.appendChild(attack)
     ul.appendChild(defense)
@@ -102,6 +111,7 @@ axios.get("https://pokeapi.co/api/v2/pokemon/562/")
 }).catch((error) => {
     console.log(error)
 })
+
 //Pokemon #711 "Gourgeist"
 axios.get("https://pokeapi.co/api/v2/pokemon/711/")
 .then((response) => {
@@ -109,12 +119,14 @@ axios.get("https://pokeapi.co/api/v2/pokemon/711/")
     let ul = document.querySelector("#gourgeistDiv")
 
     let pokeData = response.data
+    let name = document.createElement("li")
     let hp = document.createElement("li")
     let attack = document.createElement("li")
     let defense = document.createElement("li")
     let abilities = document.createElement("li")
 
     let gourgeist = new Pokemon(
+        pokeData.name,
         pokeData.stats[5].base_stat,
         pokeData.stats[4].base_stat,
         pokeData.stats[3].base_stat,
@@ -126,11 +138,13 @@ axios.get("https://pokeapi.co/api/v2/pokemon/711/")
 
     pokeData.innerHTML = gourgeist.data
 
+    name.innerHTML = "Name: " + gourgeist.name
     hp.innerHTML = "HP: " + gourgeist.hp
     attack.innerHTML = "ATTACK: " + gourgeist.attack
     defense.innerHTML = "DEFENSE: " + gourgeist.defense
     abilities.innerHTML = "ABILITIES: " + gourgeist.abilities
 
+    ul.appendChild(name)
     ul.appendChild(hp)
     ul.appendChild(attack)
     ul.appendChild(defense)
@@ -138,7 +152,17 @@ axios.get("https://pokeapi.co/api/v2/pokemon/711/")
 
 }).catch((error) => {
     console.log(error)
+    // let li = document.createElement("li")
+    // let pokeData = response.data
 
+    // let gourgeist = new Pokemon(
+    //     pokeData.name,
+    //     pokeData.stats[5].base_stat,
+    //     pokeData.stats[4].base_stat,
+    //     pokeData.stats[3].base_stat,
+    //     pokeData.abilities[0].ability.name + ", " + pokeData.abilities[1].ability.name
+    // )
+    // console.log(gourgeist)
 })
 
 console.log(doctor.all)
